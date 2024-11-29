e15ea162a1c06d50f603a42b248b2fd1d6d3adef
Emre Alak <alakemre@gmail.com>
2024-11-29 16:21:37 +0100

    Add edit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,7 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+
     public static void main(String[] args) {
 
         Medication med1 = new Medication("Aspirin", 9.99, true);
