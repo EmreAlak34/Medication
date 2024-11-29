c60a147d95cf2316962be31e71026f5001e33878
Emre Alak <alakemre@gmail.com>
2024-11-29 17:00:11 +0100

    Add correction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,29 +3,29 @@ package org.example;
 public class Main {  // The Main class should be declared here
 
     public static void main(String[] args) {
-        // Create Medications
+        // Medikamente erstllen
         Medication med1 = new Medication("Aspirin", 9.99, true);
         Medication med2 = new Medication("Ibuprofen", 12.50, false);
         Medication med3 = new Medication("Paracetamol", 4.50, true);
 
-        // Create Pharmacy and add medications
+        // Pharmacy erstellen und Medikamente hinzufügen
         Pharmacy pharmacy = new Pharmacy();
         pharmacy.save(med1);
         pharmacy.save(med2);
         pharmacy.save(med3);
 
-        // Print total medications count
+        // Komplette Menge ausgeben
         System.out.println("Total medications: " + pharmacy.getCount());
 
-        // Find a specific medication
+        // Ein bestimmtes Medikament finden
         Medication foundMed = pharmacy.find("Aspirin");
         System.out.println("Found Medication: " + foundMed);
 
-        // Print all medications
+        // Alle Medications ausgeben
         System.out.println("All medications:");
         pharmacy.printAllMedications();
 
-        // Delete a medication
+        // Ein Medikament löschen
         pharmacy.delete("Ibuprofen");
         System.out.println("After deletion:");
         pharmacy.printAllMedications();
